476f0b0b8c76a3cf37519282e52d1cbb811705b6
Yacov Drori <yacov@drori.org>
2014-05-28 02:38:08 +0300

    create niftars table

--- app/database/migrations/2014_05_05_152446_create_niftars_table.php
+++ app/database/migrations/2014_05_05_152446_create_niftars_table.php
@@ -12,23 +12,23 @@ class CreateNiftarsTable extends Migration {
 	 */
 	public function up()
 	{
-		// Schema::create('niftars', function(Blueprint $table) {
-			// $table->increments('id');
-			// $table->string('firstname');
-			// $table->string('lastname');
-			// $table->unsignedInteger('user_id');
-			// $table->foreign('user_id')->references('id')->on('users');
-			// $table->string('image');
-			// $table->date('death_date');
-			// $table->text('description');
-			// $table->date('birthdate');
-			// $table->date('deathdate');
-			// $table->text('fathersname');
-			// $table->text('mothersname');
-			// $table->text('city');
-			// $table->text('state');
-			// $table->timestamps();
-		// });	
+		Schema::create('niftars', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('firstname');
+			$table->string('lastname');
+			$table->unsignedInteger('user_id');
+			$table->foreign('user_id')->references('id')->on('users');
+			$table->string('image');
+			$table->date('death_date');
+			$table->text('description');
+			$table->date('birthdate');
+			$table->date('deathdate');
+			$table->text('fathersname');
+			$table->text('mothersname');
+			$table->text('city');
+			$table->text('state');
+			$table->timestamps();
+		});	
 
 	}
 
@@ -39,7 +39,7 @@ class CreateNiftarsTable extends Migration {
 	 */
 	public function down()
 	{
-		//Schema::drop('niftars');
+		Schema::drop('niftars');
 	}
 
 }
