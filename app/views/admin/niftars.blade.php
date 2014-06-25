7e9db02fb3e28b490555eccc690cf682f1c44921
unknown <Student@.(none)>
2014-06-25 16:08:37 +0300

    hchc

--- app/views/admin/niftars.blade.php
+++ app/views/admin/niftars.blade.php
@@ -45,7 +45,7 @@
 		<div class="col-md-3">{{$friend->email}}</div>
 		<div class="col-md-1">
 			<div class="btn-group btn-group-xs">
-				<button type="button" class="btn btn-default" href="{{action('AdminController@delFriend')}}">
+				<button type="button" class="btn btn-default" href="admin/delFriend/{{($friend->id)}}">
 	  				<span class="glyphicon glyphicon-remove"></span>
 				</button>
 			</div>
