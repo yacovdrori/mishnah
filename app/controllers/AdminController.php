ceccf226eeeb07021c0b938a2f962bb082e5428e
yacov drori <yacov@drori.org>
2014-06-26 18:36:59 +0300

    changes

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -2,10 +2,10 @@
  
 class AdminController extends BaseController {
 
-	 public function __construct()
-    {
-        $this->beforeFilter('auth', array('except' => 'getLogin'));
-    }
+	 // public function __construct()
+  //   {
+  //       $this->beforeFilter('auth', array('except' => 'getLogin'));
+  //   }
 
 	public function getNewuser()
 	{
