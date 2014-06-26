19bd2691084da7f2877eef3678a4e9918eadf8e9
yacov drori <yacov@drori.org>
2014-06-26 18:45:44 +0300

    changes

--- app/views/emails/emailfriend.blade.php
+++ app/views/emails/emailfriend.blade.php
@@ -4,10 +4,11 @@
 		<meta charset="utf-8">
 	</head>
 	<body>
+		{{dd($invitename)}}
 		<h2>שלום {{$name}}</h2>
 
 		<div>
-			{{$invitename}} הזמין אותך ללמוד משניות לעילוי נשמת {{$relationship}} שנפטר.
+			{{ $invitename}} הזמין אותך ללמוד משניות לעילוי נשמת {{$relationship}} שנפטר.
 			להרשמה ובחירת מסכת, אנא לחץ על הקישור הבא.
 			{{ HTML::link("admin/newuser/" . $nid, "לימוד לעילוי נשמת " . $nfirstname . " " . $nlastname . " בן " .
     	 $nfathersname . " ו" .$nmothersname) }}.
