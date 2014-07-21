afccef730e5fe6dd28b507aacb65b16a81705529
yacov drori <yacov@yacov-Lenovo-B590.(none)>
2014-07-21 19:54:04 +0300

    moved link rels to header

--- app/views/header.blade.php
+++ app/views/header.blade.php
@@ -1,3 +1,5 @@
-              <title>לימוד משניות לעילוי נשמת</title>
  				<?= stylesheet_link_tag() ?>
 				<?= javascript_include_tag() ?>
+				<script src = "http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+                <script src = "js/bootstrap.js"></script>
+                <title>לימוד משניות לעילוי נשמת</title>
