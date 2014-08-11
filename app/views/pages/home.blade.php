dce2b6664fdbdd4e27dc5f9935b674bf79940f0a
Yacov Drori <yacov@drori.org>
2014-08-11 20:54:50 +0300

    new

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -1,16 +1,24 @@
 @extends('index')
 @section('content')
-                        <div class = "container">               
-                        	<div class = "jumbotron">
-                                <center><h1>הפתרון המושלם לניהול לימוד משניות לזכר יקירכם</h1>
-                                <p>מערכת מלאה לעזרה בחלוקת המשניות ללומדים, למסירת תזכורות ולהנצחת הנפטרים</p>
-                                
+	<section id="home" name="home"></section>
+	<div id="headerwrap">
+	    <div class="container">
+	    	<div class="row centered">
+	    		<div class="col-lg-12">
+                    <center><h1>הפתרון המושלם לניהול לימוד משניות לזכר יקירכם</h1>
+                    <p>מערכת מלאה לעזרה בחלוקת המשניות ללומדים, למסירת תזכורות ולהנצחת הנפטרים</p>
+                </div>
+                <div class="col-lg-8">
+                	{{HTML::image('images/candle.jpg', 'יזכור', array('class'=>'img-responsive'))}}
+                </div>
+                <div class="col-lg-4">
                                 	{{HTML::link('/users/register','הרשם')}}<br />
                                 	הוסף את שם הניפטר<br />
                                 	הזמן חברים ובני משפחה
+                	
+                </div>		
 
                                 </center>
-                        	</div>
                         </div>
 
 @stop
